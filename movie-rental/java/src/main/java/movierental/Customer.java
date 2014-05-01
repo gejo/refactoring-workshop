8fcd89a7295f4b6393badff5786d0bc240babc7a
gejo <joy.ge@oocl.com>
2014-05-01 22:06:19 +0800

    move method calculate amount from Customer to Rental

--- movie-rental/java/src/main/java/movierental/Customer.java
+++ movie-rental/java/src/main/java/movierental/Customer.java
@@ -25,7 +25,7 @@ public class Customer {
         String result = "Rental Record for " + getName() + "\n";
 
         for (Rental rental: rentals) {
-            double thisAmount = calculateAmount(rental);
+            double rentalAmount = rental.calculateAmount();
 
             // add frequent renter points
             frequentRenterPoints++;
@@ -34,8 +34,8 @@ public class Customer {
                 frequentRenterPoints++;
 
             // show figures for this rental
-            result += "\t" + rental.getMovie().getTitle() + "\t" + String.valueOf(thisAmount) + "\n";
-            totalAmount += thisAmount;
+            result += "\t" + rental.getMovie().getTitle() + "\t" + String.valueOf(rentalAmount) + "\n";
+            totalAmount += rentalAmount;
         }
 
         // add footer lines
@@ -45,25 +45,4 @@ public class Customer {
         return result;
     }
 
-    private double calculateAmount(Rental rental) {
-        double amount = 0;
-
-        //determine amounts for the given rental
-        switch (rental.getMovie().getPriceCode()) {
-            case Movie.REGULAR:
-                amount += 2;
-                if (rental.getDaysRented() > 2)
-                    amount += (rental.getDaysRented() - 2) * 1.5;
-                break;
-            case Movie.NEW_RELEASE:
-                amount += rental.getDaysRented() * 3;
-                break;
-            case Movie.CHILDRENS:
-                amount += 1.5;
-                if (rental.getDaysRented() > 3)
-                    amount += (rental.getDaysRented() - 3) * 1.5;
-                break;
-        }
-        return amount;
-    }
 }
--- movie-rental/java/src/main/java/movierental/Rental.java
+++ movie-rental/java/src/main/java/movierental/Rental.java
@@ -20,4 +20,26 @@ public class Rental {
     public Movie getMovie() {
         return _movie;
     }
+
+    double calculateAmount() {
+        double amount = 0;
+
+        //determine amounts for the given rental
+        switch (getMovie().getPriceCode()) {
+            case Movie.REGULAR:
+                amount += 2;
+                if (getDaysRented() > 2)
+                    amount += (getDaysRented() - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                amount += getDaysRented() * 3;
+                break;
+            case Movie.CHILDRENS:
+                amount += 1.5;
+                if (getDaysRented() > 3)
+                    amount += (getDaysRented() - 3) * 1.5;
+                break;
+        }
+        return amount;
+    }
 }
